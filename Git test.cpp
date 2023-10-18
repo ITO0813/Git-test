3a1b79f42a636d9e3bf265321047938c21d5cec0
hibiki <k022g1084@g.neec.ac.jp>
2023-10-18 10:22:43 +0900

    PullRequestの追加

--- Git test.cpp
+++ Git test.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello Git Hub!\n";
 
     std::cout << "New branch\n";
+
+    std::cout << "PullRequest branch changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
